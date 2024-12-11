883c5abc738308098cb95bbbf6f81d19327d37c4
Ivan Shakhov <48312216+Tyrge@users.noreply.github.com>
2024-12-11 21:39:54 +0300

    Add files via upload

---
+++ Лабораторная работа 3/Поиск индекса элемента в списке товаров.py
@@ -0,0 +1,15 @@
+def testlist(list, find):
+    if find in list:
+        index = list.index(find)
+    else:
+        index = None
+    return(index)
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = testlist(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
